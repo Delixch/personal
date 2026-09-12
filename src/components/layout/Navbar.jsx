68a66736e42e19bc8bc80369a338a4488f8dc0af
Adnan Aydin <xanadu.ch@gmail.com>
2026-09-12 23:33:06 +0200

    style: apply #180E0E background to top navbar

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -54,7 +54,7 @@ export const Navbar = ({
   };
 
   return (
-    <header className="sticky top-0 z-40 w-full border-b border-line bg-surface">
+    <header className="sticky top-0 z-40 w-full border-b border-line bg-navbar">
       <div className="flex h-16 items-center justify-between px-4 sm:px-6 lg:px-8 max-w-7xl mx-auto">
 
         <div
